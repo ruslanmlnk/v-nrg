8bdb40a68fa79fdefb5536e68ed9f3611638e3c2
ruslanmlnk <kanevgoga2@gmail.com>
2026-03-30 08:45:04 +0200

    fix

--- src/app/(frontend)/components/mainPage/Hero.tsx
+++ src/app/(frontend)/components/mainPage/Hero.tsx
@@ -11,20 +11,21 @@ const Hero = () => {
                     <h1 className="text-[56px] font-semibold leading-[125%] text-white">
                         Професійні апарати вакуумного масажу V-NRG
                     </h1>
-                    <p className="mt-4 text-[18px] leading-[165%] text-white font-medium">
+                    <p className="mt-4 text-[18px] font-medium leading-[165%] text-white">
                         Допомагають працювати з болем, набряками та станом шкіри. Швидкий запуск у роботу,
                         навчання та підтримка від виробника
                     </p>
                 </div>
-                <button className='relative mt-[164px] bg-white text-[#22354A] px-6 py-3 rounded-[40px] flex items-center h-[54px]'>
-                    <div className='text-[18px] text-medium leading-[125%]'>Отримати консультацію</div>
-                    <div className='absolute right-0 translate-x-[100%] w-[54px] h-[54px] bg-[#4FACF5] rounded-full flex items-center justify-center'>
+                <button className='group relative mt-[164.5px] flex h-[54px] items-center rounded-[40px] bg-white px-6 text-[#22354A] transition-colors duration-300 hover:text-white'>
+                    <div className='relative z-10 text-[18px] font-medium leading-[165%]'>Отримати консультацію</div>
+                    <div className='pointer-events-none absolute inset-y-0 right-[-54px] w-[54px] rounded-full bg-[#4FACF5] transition-[width,border-radius] duration-300 ease-out group-hover:w-[calc(100%+54px)] group-hover:rounded-[40px]'></div>
+                    <div className='pointer-events-none absolute right-[-54px] top-0 z-10 flex h-[54px] w-[54px] items-center justify-center'>
                         <Image src={arrow_side} alt="arrow" />
                     </div>
                 </button>
             </div>
             <div className='rounded-[20px] relative overflow-hidden'>
-                <Image src={hero_bg} alt="arrow" fill />
+                <Image src={hero_bg} alt="arrow" />
             </div>
         </section>
     );
