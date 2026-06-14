f3b56339afaaf00c83bae98a81d39c8cd0d0e3ec
ruslanmlnk <kanevgoga2@gmail.com>
2026-06-14 06:36:09 +0200

    place home blog heading before cards

--- src/app/(frontend)/components/mainPage/HomeBlogSection.tsx
+++ src/app/(frontend)/components/mainPage/HomeBlogSection.tsx
@@ -10,25 +10,25 @@ export default function HomeBlogSection({ articles }: { articles: BlogCardData[]
   return (
     <section className="px-6 pt-[100px] lg:px-[100px]">
       <div className="mx-auto flex max-w-[1240px] flex-col gap-6 md:gap-12">
-        <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
-          {articles.map((article) => (
-            <BlogCard key={article.id} {...article} />
-          ))}
-        </div>
-
-        <div className="flex flex-col items-center gap-8 md:grid md:grid-cols-2 md:items-center md:gap-5">
-          <ArrowPillButton
-            href="/blog"
-            isDark
-            className="mr-[50px] md:mr-[54px] md:justify-self-start"
-          >
-            Усі статті
-          </ArrowPillButton>
+        <div className="flex flex-col items-center gap-8 md:flex-row md:justify-between md:gap-12">
           <SectionHeading
             eyebrow="Блог"
             title="Блог та корисні матеріали"
             className="items-center text-center md:items-start md:text-left"
           />
+          <ArrowPillButton
+            href="/blog"
+            isDark
+            className="mr-[50px] shrink-0 md:mr-[54px]"
+          >
+            Усі статті
+          </ArrowPillButton>
+        </div>
+
+        <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+          {articles.map((article) => (
+            <BlogCard key={article.id} {...article} />
+          ))}
         </div>
       </div>
     </section>
